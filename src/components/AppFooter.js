3cf508fb38dcd1a6afd4bbe049e781814bdd4b06
devenkshp <devenkshp@users.noreply.github.com>
2019-01-23 18:35:17 +0545

    New commit

---
+++ src/components/AppFooter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppFooter() {
+  return <p className="text-secondary">@devenkshp</p>;
+}
+
+export default AppFooter;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import AppFooter from "./components/AppFooter";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<AppFooter />, document.getElementById("footer"));
 
 serviceWorker.unregister();
